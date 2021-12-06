4b1f3206e616bfbf2a492f84ca0a325104182e48
Nikos <printezisn@gmail.com>
2021-12-07 01:05:28 +0200

    Add solutions for day 6 challenge

---
+++ solutions/day6/solution2.rb
@@ -0,0 +1,18 @@
+states = File.read(File.join(File.dirname(__FILE__), 'input.txt')).split(',').map { |state| [state.to_i, 1] }
+
+256.times do
+  new_fish = 0
+
+  states.each_with_index do |_, index|
+    if states[index][0] == 0
+      states[index][0] = 6
+      new_fish += states[index][1]
+    else
+      states[index][0] -= 1
+    end
+  end
+
+  states << [8, new_fish]
+end
+
+puts states.sum(&:last)
