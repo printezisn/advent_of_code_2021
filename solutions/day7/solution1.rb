96ff150da6061ab86812895c92bd0467d4e1c4af
Nikos <printezisn@gmail.com>
2021-12-07 22:41:23 +0200

    Add solutions for day 7 challenge

---
+++ solutions/day7/solution1.rb
@@ -0,0 +1,15 @@
+positions = File.read(File.join(File.dirname(__FILE__), 'input.txt')).split(',').map(&:to_i)
+
+min_fuel = -1
+(positions.min..positions.max).each do |point|
+  point_fuel = 0
+
+  positions.each do |position|
+    diff = (point - position).abs
+    point_fuel += diff
+  end
+
+  min_fuel = point_fuel if min_fuel == -1 || point_fuel < min_fuel
+end
+
+puts min_fuel
