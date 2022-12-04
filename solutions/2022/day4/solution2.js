4c6ced388b76e36bec8779cd1f73311adf683eb1
Nikos <printezisn@gmail.com>
2022-12-04 07:22:46 +0200

    Add solution to day 4 challenge in 2022

---
+++ solutions/2022/day4/solution2.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const fileContent = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf8', flag: 'r' });
+const lines = fileContent.split('\n');
+let total = 0;
+
+lines.forEach(line => {
+  const [range1, range2] = line.split(',');
+  const [range1Start, range1End] = range1.split('-').map(Number);
+  const [range2Start, range2End] = range2.split('-').map(Number);
+
+  if (range1Start >= range2Start && range1Start <= range2End) {
+    total++;
+  } else if (range1End >= range2Start && range1End <= range2End) {
+    total++;
+  } else if (range2Start >= range1Start && range2Start <= range1End) {
+    total++;
+  } else if (range2End >= range1Start && range2End <= range1End) {
+    total++;
+  }
+})
+
+console.log(total);
