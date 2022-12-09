8e538993d4c604c302ee13ce26f4d29d23ecd930
Nikos Printezis <npr@captureone.com>
2022-12-09 12:01:18 +0200

    Add full solution to day 9 challenge in 2022

--- solutions/2022/day9/solution1.js
+++ solutions/2022/day9/solution1.js
@@ -5,59 +5,53 @@ const isAdjacent = (head, tail) => {
   return Math.abs(head[0] - tail[0]) <= 1 && Math.abs(head[1] - tail[1]) <= 1;
 }
 
+const moveTail = (head, tail) => {
+  if (isAdjacent(head, tail)) return;
+
+  if (tail[0] === head[0]) {
+    tail[1] = (head[1] < tail[1]) ? (head[1] + 1) : (head[1] - 1);
+  } else if (tail[1] === head[1]) {
+    tail[0] = (head[0] < tail[0]) ? (head[0] + 1) : (head[0] - 1);
+  } else if (head[1] < tail[1] && head[0] > tail[0]) {
+    tail[0]++;
+    tail[1]--;
+  } else if (head[1] < tail[1] && head[0] < tail[0]) {
+    tail[0]--;
+    tail[1]--;
+  } else if (head[1] > tail[1] && head[0] > tail[0]) {
+    tail[0]++;
+    tail[1]++;
+  } else if (head[1] > tail[1] && head[0] < tail[0]) {
+    tail[0]--;
+    tail[1]++;
+  }
+}
+
+const moveAllTails = (tails, visited) => {
+  for (let i = 1; i < tails.length; i++) {
+    moveTail(tails[i - 1], tails[i], visited);
+  }
+
+  visited[tails[tails.length - 1]] = true;
+}
+
 const fileContent = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf8', flag: 'r' });
 const lines = fileContent.split('\n');
 
-let head = [0, 0], tail = [0, 0];
+const tails = [...Array(2)].map(() => [0, 0]);
 const visited = { [[0, 0].toString()]: true };
 
 lines.forEach(line => {
   const [move, numStr] = line.trim().split(' ');
   const num = Number(numStr);
 
-  if (move === 'R') {
-    head[0] += num;
-  } else if (move === 'U') {
-    head[1] -= num;
-  } else if (move === 'L') {
-    head[0] -= num;
-  } else if (move === 'D') {
-    head[1] += num;
-  }
+  for (let i = 0; i < num; i++) {
+    if (move === 'R') tails[0][0]++;
+    else if (move === 'U') tails[0][1]--;
+    else if (move === 'L') tails[0][0]--;
+    else if (move === 'D') tails[0][1]++;
 
-  if (isAdjacent(head, tail)) return;
-
-  while (head[1] < tail[1] - 1) {
-    if (head[0] !== tail[0]) {
-      tail[0] = head[0];
-    }
-
-    tail[1]--;
-    visited[tail.toString()] = true;
-  }
-  while (head[1] > tail[1] + 1) {
-    if (head[0] !== tail[0]) {
-      tail[0] = head[0];
-    }
-
-    tail[1]++;
-    visited[tail.toString()] = true;
-  }
-  while (head[0] > tail[0] + 1) {
-    if (head[1] !== tail[1]) {
-      tail[1] = head[1];
-    }
-
-    tail[0]++;
-    visited[tail.toString()] = true;
-  }
-  while (head[0] < tail[0] - 1) {
-    if (head[1] !== tail[1]) {
-      tail[1] = head[1];
-    }
-
-    tail[0]--;
-    visited[tail.toString()] = true;
+    moveAllTails(tails, visited);
   }
 });
 
--- solutions/2022/day9/solution2.js
+++ solutions/2022/day9/solution2.js
@@ -5,80 +5,54 @@ const isAdjacent = (head, tail) => {
   return Math.abs(head[0] - tail[0]) <= 1 && Math.abs(head[1] - tail[1]) <= 1;
 }
 
-const moveTail = (head, tail, visited) => {
+const moveTail = (head, tail) => {
   if (isAdjacent(head, tail)) return;
 
-  while (head[1] < tail[1] - 1) {
-    if (head[0] !== tail[0]) {
-      tail[0] = head[0];
-    }
-
-    tail[1]--;
-    visited[tail.toString()] = true;
-  }
-  while (head[1] > tail[1] + 1) {
-    if (head[0] !== tail[0]) {
-      tail[0] = head[0];
-    }
-
-    tail[1]++;
-    visited[tail.toString()] = true;
-  }
-  while (head[0] > tail[0] + 1) {
-    if (head[1] !== tail[1]) {
-      tail[1] = head[1];
-    }
-
+  if (tail[0] === head[0]) {
+    tail[1] = (head[1] < tail[1]) ? (head[1] + 1) : (head[1] - 1);
+  } else if (tail[1] === head[1]) {
+    tail[0] = (head[0] < tail[0]) ? (head[0] + 1) : (head[0] - 1);
+  } else if (head[1] < tail[1] && head[0] > tail[0]) {
     tail[0]++;
-    visited[tail.toString()] = true;
-  }
-  while (head[0] < tail[0] - 1) {
-    if (head[1] !== tail[1]) {
-      tail[1] = head[1];
-    }
-
+    tail[1]--;
+  } else if (head[1] < tail[1] && head[0] < tail[0]) {
     tail[0]--;
-    visited[tail.toString()] = true;
+    tail[1]--;
+  } else if (head[1] > tail[1] && head[0] > tail[0]) {
+    tail[0]++;
+    tail[1]++;
+  } else if (head[1] > tail[1] && head[0] < tail[0]) {
+    tail[0]--;
+    tail[1]++;
   }
 }
 
-const moveAllTails = (head, tails, visited) => {
-  for (let i = 0; i < tails.length; i++) {
-    moveTail(i === 0 ? head : tails[i - 1], tails[i], visited);
+const moveAllTails = (tails, visited) => {
+  for (let i = 1; i < tails.length; i++) {
+    moveTail(tails[i - 1], tails[i], visited);
   }
-  console.log(head, tails);
 
+  visited[tails[tails.length - 1]] = true;
 }
 
 const fileContent = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf8', flag: 'r' });
 const lines = fileContent.split('\n');
 
-let head = [0, 0], tails = [...Array(10)].map(() => [0, 0]);
+const tails = [...Array(10)].map(() => [0, 0]);
 const visited = { [[0, 0].toString()]: true };
 
-lines.slice(0, 2).forEach(line => {
+lines.forEach(line => {
   const [move, numStr] = line.trim().split(' ');
   const num = Number(numStr);
 
-  if (move === 'R') {
-    for (let i = 0; i < num; i++) {
-      head[0]++;
-      moveAllTails(head, tails, visited);
-    }
-  } else if (move === 'U') {
-    for (let i = 0; i < num; i++) {
-      head[1]--;
-      moveAllTails(head, tails, visited);
-    }
-  } else if (move === 'L') {
-    for (let i = 0; i < num; i++) {
-      head[0]--;
-      moveAllTails(head, tails, visited);
-    }
-  } else if (move === 'D') {
-    for (let i = 0; i < num; i++) {
-      head[1]++;
-      moveAllTails(head, tails, visited);
-    }
+  for (let i = 0; i < num; i++) {
+    if (move === 'R') tails[0][0]++;
+    else if (move === 'U') tails[0][1]--;
+    else if (move === 'L') tails[0][0]--;
+    else if (move === 'D') tails[0][1]++;
+
+    moveAllTails(tails, visited);
   }
 });
+
+console.log(Object.keys(visited).length);
