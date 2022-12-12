cb153b14cfb670b62fe401b8e969a76e14a8f2fd
Nikos Printezis <npr@captureone.com>
2022-12-12 09:46:18 +0200

    Optimize solution for day 12 in 2022

--- solutions/2022/day12/solution2.js
+++ solutions/2022/day12/solution2.js
@@ -8,50 +8,44 @@ const getElevation = (ch) => {
   return ch.charCodeAt(0) - 'a'.charCodeAt(0);
 }
 
-const calculateMinSteps = (grid, startPosition, endPosition) => {
-  const nodesToVisit = [startPosition], visitedNodes = { [startPosition.toString()]: 0 };
-
-  while (nodesToVisit.length > 0) {
-    const node = nodesToVisit.shift();
-    const totalSteps = visitedNodes[node.toString()];
-    const [row, col] = node;
-    const elevation = getElevation(grid[row][col]);
-    const neighbours = [[row - 1, col], [row + 1, col], [row, col - 1], [row, col + 1]];
-
-    neighbours.forEach(nextNode => {
-      const [nextRow, nextCol] = nextNode;
-      if (nextRow < 0 || nextRow >= grid.length || nextCol < 0 || nextCol >= grid[nextRow].length) return;
-      
-      const nextElevation = getElevation(grid[nextRow][nextCol]);
-      if (nextElevation - elevation > 1) return;
-
-      if (visitedNodes[nextNode.toString()] == null || visitedNodes[nextNode.toString()] > totalSteps + 1) {
-        nodesToVisit.push([nextRow, nextCol]);
-        visitedNodes[nextNode.toString()] = totalSteps + 1;
-      }
-    });
-  }
-
-  return visitedNodes[endPosition.toString()];
-}
-
 const fileContent = fs.readFileSync(path.join(__dirname, 'input.txt'), { encoding: 'utf8', flag: 'r' });
 const grid = fileContent.split('\n');
-let startPositions = [], endPosition = [0, 0], i, j;
+let startPosition = null, endPositions = {}, i, j, min = null;
 
 for (i  = 0; i < grid.length; i++) {
   for (j = 0; j < grid[i].length; j++) {
-    if (grid[i][j] === 'S' || grid[i][j] === 'a') startPositions.push([i, j]);
-    else if (grid[i][j] === 'E') endPosition = [i, j];
+    if (grid[i][j] === 'E') startPosition = [i, j];
+    else if (grid[i][j] === 'S' || grid[i][j] === 'a') endPositions[[i, j].toString()] = true;
   }
 }
 
-const minSteps = startPositions.reduce((min, startPosition) => {
-  const result = calculateMinSteps(grid, startPosition, endPosition);
-  if (result == null) return min;
-  if (min == null) return result;
+const nodesToVisit = [startPosition], visitedNodes = { [startPosition.toString()]: 0 };
 
-  return Math.min(min, result);
-}, null);
+while (nodesToVisit.length > 0) {
+  const node = nodesToVisit.shift();
+  const totalSteps = visitedNodes[node.toString()];
 
-console.log(minSteps);
+  if (endPositions[node.toString()]) {
+    min = (min === null) ? totalSteps : Math.min(min, totalSteps);
+    continue;
+  }
+
+  const [row, col] = node;
+  const elevation = getElevation(grid[row][col]);
+  const neighbours = [[row - 1, col], [row + 1, col], [row, col - 1], [row, col + 1]];
+
+  neighbours.forEach(nextNode => {
+    const [nextRow, nextCol] = nextNode;
+    if (nextRow < 0 || nextRow >= grid.length || nextCol < 0 || nextCol >= grid[nextRow].length) return;
+
+    const nextElevation = getElevation(grid[nextRow][nextCol]);
+    if (nextElevation - elevation < -1) return;
+
+    if (visitedNodes[nextNode.toString()] == null || visitedNodes[nextNode.toString()] > totalSteps + 1) {
+      nodesToVisit.push([nextRow, nextCol]);
+      visitedNodes[nextNode.toString()] = totalSteps + 1;
+    }
+  });
+}
+
+console.log(min);
