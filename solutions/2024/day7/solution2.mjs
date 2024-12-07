ffba59fbdb2cdb23aee54a78ef3ab8b5f47595fd
printezisn <printezisn@gmail.com>
2024-12-07 10:00:31 +0200

    Add solution to day 7 in 2024

---
+++ solutions/2024/day7/solution2.mjs
@@ -0,0 +1,41 @@
+import fs from "fs";
+import path from "path";
+
+const checkIfEquationIsTrue = (equation, total = equation[1], index = 2) => {
+  if (index >= equation.length) {
+    return equation[0] === total;
+  }
+
+  return (
+    checkIfEquationIsTrue(equation, total + equation[index], index + 1) ||
+    checkIfEquationIsTrue(equation, total * equation[index], index + 1) ||
+    checkIfEquationIsTrue(
+      equation,
+      Number(`${total}${equation[index]}`),
+      index + 1
+    )
+  );
+};
+
+const equations = [];
+let result = 0;
+
+const lines = fs
+  .readFileSync(path.resolve(import.meta.dirname, "input.txt"))
+  .toString()
+  .split("\n");
+lines.forEach((line) => {
+  const parts = line
+    .trim()
+    .split(" ")
+    .map((n) => parseInt(n));
+  equations.push(parts);
+});
+
+equations.forEach((equation) => {
+  if (checkIfEquationIsTrue(equation)) {
+    result += equation[0];
+  }
+});
+
+console.log(result);
